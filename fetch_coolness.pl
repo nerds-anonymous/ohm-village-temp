41aed7a95871cabe71d0540a98734ea47dd0874d
Sebastian Krohn <seb@gaia.sunn.de>
2013-09-28 13:05:17 +0200

    small cleanup for better looks

--- fetch_coolness.pl
+++ fetch_coolness.pl
@@ -8,7 +8,7 @@ use HTML::TableExtract;
 
 my $te = HTML::TableExtract->new( depth => 0 );
 my $ua = LWP::UserAgent->new(agent => 'Mozilla 5.0',
-								timeout => 30);
+	timeout => 30);
 
 my $r = $ua->get('http://151.218.112.31/');
 die $r->status_line if ! $r->is_success;
